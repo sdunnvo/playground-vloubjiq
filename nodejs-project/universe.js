880a48491b1887c78ab330b9012d8ced65ce458c
sdunnvo <124534566+sdunnvo@users.noreply.github.com>
2023-10-18 07:00:57 -0700

    Update universe.js
    _.REDUCE me bro!

--- nodejs-project/universe.js
+++ nodejs-project/universe.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 function countAllStars(galaxies) {
   console.log("\n\n--> All the Galaticals :: " + galaxies);
   var totalStars = 0;
@@ -6,6 +8,9 @@ function countAllStars(galaxies) {
     totalStars += stars; // fix me
   }, this);
 
+  var reducted = _.reduce(galaxies, (iter, accum) => { return acum += iter }, 0);
+  console.log("\n\n----------\nREDUCTED calulations :: " + reducted);
+  
   console.log("Final calulations :: " + totalStars);
   return totalStars;
 }
