e28c9d3d72b81d2c9faac0aca7cf0c65dc3feaac
sdunnvo <124534566+sdunnvo@users.noreply.github.com>
2023-10-18 06:48:57 -0700

    Update universe.js
    match MASTER branch.

--- nodejs-project/universe.js
+++ nodejs-project/universe.js
@@ -1,9 +1,12 @@
 function countAllStars(galaxies) {
-  console.log("Galaticals :: " + galaxies);
+  console.log("\n\n--> All the Galaticals :: " + galaxies);
   var totalStars = 0;
+  
   galaxies.forEach(function(stars) {
     totalStars += stars; // fix me
   }, this);
+
+  console.log("Final calulations :: " + totalStars);
   return totalStars;
 }
 
